8a5f06511c28fd9f5fc05d2b142fb3a8dc0e02a3
Алексей <mail@ashatrov.ru>
2024-12-01 12:58:22 +0300

    фронт-энд с бэк-ендом

--- console/migrations/m241130_044359_create_url_table.php
+++ console/migrations/m241130_044359_create_url_table.php
@@ -14,21 +14,21 @@ class m241130_044359_create_url_table extends Migration
     public function safeUp()
     {
         $this->createTable(Url::tableName(), [
-			'id' => $this->primaryKey() // ID
+            'id' => $this->primaryKey() // ID
             , 'href' => $this->string()->notNull()->unique() // url для проверки
-			, 'frequency' => $this->integer()->unsigned()->notNull()->defaultValue(5) // частота проверки, минуты
-			, 'repetitions' => $this->integer()->notNull()->defaultValue(0) // количество повторов в случае ошибки, -1=бесконечность
-			, 'delay' => $this->integer()->unsigned()->notNull()->defaultValue(5), // задержка в минутах между переповторами
+            , 'frequency' => $this->integer()->unsigned()->notNull()->defaultValue(5) // частота проверки, минуты
+            , 'repetitions' => $this->integer()->notNull()->defaultValue(0) // количество повторов в случае ошибки, -1=бесконечность
+            , 'delay' => $this->integer()->unsigned()->notNull()->defaultValue(5), // задержка в минутах между переповторами
         ]);
 
-		$this->createIndex('idx-href-repetitions', Url::tableName(), ['href', 'repetitions',]);
+        $this->createIndex('idx-href-repetitions', Url::tableName(), ['href', 'repetitions',]);
 
-		$this->addCommentOnTable(Url::tableName(), 'ссылки на проверку');
-		$this->addCommentOnColumn(Url::tableName(), 'id', 'ID');
-		$this->addCommentOnColumn(Url::tableName(), 'href', 'url для проверки');
-		$this->addCommentOnColumn(Url::tableName(), 'frequency', 'частота проверки, минуты');
-		$this->addCommentOnColumn(Url::tableName(), 'repetitions', 'количество повторов в случае ошибки, -1=бесконечность');
-		$this->addCommentOnColumn(Url::tableName(), 'delay', 'задержка в минутах между переповторами');
+        $this->addCommentOnTable(Url::tableName(), 'ссылки на проверку');
+        $this->addCommentOnColumn(Url::tableName(), 'id', 'ID');
+        $this->addCommentOnColumn(Url::tableName(), 'href', 'url для проверки');
+        $this->addCommentOnColumn(Url::tableName(), 'frequency', 'частота проверки, минуты');
+        $this->addCommentOnColumn(Url::tableName(), 'repetitions', 'количество повторов в случае ошибки, -1=бесконечность');
+        $this->addCommentOnColumn(Url::tableName(), 'delay', 'задержка в минутах между переповторами');
     }
 
     /**
--- console/migrations/m241130_134515_create_logs_table.php
+++ console/migrations/m241130_134515_create_logs_table.php
@@ -15,15 +15,15 @@ class m241130_134515_create_logs_table extends Migration
     {
         $this->createTable(LogModel::tableName(), [
             'id' => $this->primaryKey()->comment('ID')
-			, 'url_id' => $this->integer()->notNull()->comment('ID url')
-			, 'code' => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('HTTP-код')
-			, 'body' => $this->text()->comment('ответ')
-			, 'created_at' => $this->timestamp()->notNull()->comment('дата-время запроса'),
+            , 'url_id' => $this->integer()->notNull()->comment('ID url')
+            , 'code' => $this->integer()->unsigned()->notNull()->defaultValue(0)->comment('HTTP-код')
+            , 'body' => $this->text()->comment('ответ')
+            , 'created_at' => $this->timestamp()->notNull()->comment('дата-время запроса'),
         ]);
 
-		$this->createIndex('idx-created_at', LogModel::tableName(), 'created_at');
-		$this->addCommentOnTable(LogModel::tableName(), 'журнал');
-		$this->addForeignKey('fk-log-url', LogModel::tableName(), 'url_id', Url::tableName(), 'id', 'CASCADE');
+        $this->createIndex('idx-created_at', LogModel::tableName(), 'created_at');
+        $this->addCommentOnTable(LogModel::tableName(), 'журнал');
+        $this->addForeignKey('fk-log-url', LogModel::tableName(), 'url_id', Url::tableName(), 'id', 'CASCADE');
     }
 
     /**
